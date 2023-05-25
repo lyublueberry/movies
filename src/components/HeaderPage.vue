99f78cb2695a2445cffadcf7dc479bc575014a9d
Lara87 <lara.87@bk.ru>
2023-05-25 23:54:15 +0300

    change

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -3,7 +3,7 @@
         <b-navbar type="dark" variant="dark">
             <b-navbar-nav>
                 <b-nav-item tag="router-link" :to="{ path: '/' }">Список популярных фильмов</b-nav-item>
-                <b-nav-item tag="router-link" :to="{ path: '/cinema' }">Жанры фильмов</b-nav-item>
+                <b-nav-item tag="router-link" :to="{ path: '/genres' }">Жанры фильмов</b-nav-item>
             </b-navbar-nav>
         </b-navbar>
     </div>
